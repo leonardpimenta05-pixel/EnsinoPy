55c9a02fc97e9e58cf73d3b032b9dec16b2bf426
LeonardoPimenta <leonardpimenta05@gmail.com>
2026-09-04 00:17:58 -0300

    Meus exercicios e praticas para meu aprendizados, corrigidos e analisados.

---
+++ Pratica/exercicio2.py
@@ -0,0 +1,17 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+    if ' ' in nome:
+        print('Seu nome contem espaços')
+    else:
+        print('Seu nome NAO contem espaços')
+
+    print(f'Seu nome tem {len(nome)} letras')
+    print(f'a primeira letra do seu nome é {nome[0]} ')        
+    print(f'a ultima letra do seu nome é {nome[-1]} ')
+
+else:
+    print('Desculpe, voce deixou campos vazios')     
