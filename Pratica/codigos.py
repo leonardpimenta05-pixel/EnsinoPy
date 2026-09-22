48a2632d3ce214597bc140a287bb57d6b6835fc8
LeonardoPimenta <leonardpimenta05@gmail.com>
2026-09-22 16:20:04 -0300

    Mais 2 exercicios feitos e seguimos praticando e evoluindo.

--- Pratica/codigos.py
+++ Pratica/codigos.py
@@ -1,3 +1,6 @@
+''''
+PRATICANDO COM GPT, EVOLUINDO GRADUALMENTE
+
 num = int(input('Digite um Numero: '))
 
 
@@ -15,6 +18,41 @@ if num % 2 == 0:
 else:
     print(f'O numero {num} é impar.')    
 
+'''
+
+
+'''
+num1 = float(input('Digite sua nota: '))
+num2 = float(input('Digite sua outra nota: '))
+media = (num1 + num2) / 2
+
+print(f'Sua media foi {media:.2f}')
+
+if media >= 7:
+    print('Voce foi aprovado! Parabens!!')
+
+elif media >=5:
+    print('Voce esta de recuperaçao.')
+
+else:
+    print('Voce foi reprovado, Tente novamente.')
+            
+'''
+
+login = 'caramelo'
+password = '200605'   
+
+acesso = input('Digite o seu login: ')
+senha = input('Digite sua senha: ')
+
+if login == acesso and password == senha:
+    print('Login Realizado com sucesso, seja bem vindo!!')
+
+elif login == acesso and password != senha:
+    print('senha Incorreta, tente novamente.')
+
+else:
+    print('Usuario nao encontrado, tente novamente')    
 
     
     
